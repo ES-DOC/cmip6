43214df343178e8b70261da4cc0a95d2da8e906e
Sadie Louise Bartholomew <sadie.bartholomew@ncas.ac.uk>
2021-05-07 15:10:24 +0100

    1. Improve mapping to inputs for machine spreadsheet to CIM script

--- lib/machines/generate_cim_via_json.py
+++ lib/machines/generate_cim_via_json.py
@@ -14,7 +14,20 @@ import json
 from openpyxl import load_workbook
 
 
-INPUT_LABELS = (
+LABEL_COLUMN = 0  # i.e. index in A-Z of columns as tuple, so "A"
+INPUT_COLUMN = 1  # i.e. "B"
+
+# Usually <500, but in case of much cell copying and all exps and models listed
+MAX_ROW = 600
+
+# Is this (first one) about right? Is a guess based on seen gen'd model lists
+MAX_NUMBER_MODELS_PER_INSTITUTE = 30  # TODO: replace with inst-specific value
+MAX_NUMBER_MIPS = 22  # TODO: replace with inst-specific value
+
+
+# Tuples give digits corresopnding to question labels to match to user inputs,
+# e.g. (1, 1, 1) -> question "1.1.1" or "1.1.1 *"
+input_labels = [
     # Identity:
     (1, 1, 1),
     (1, 1, 2),
@@ -100,17 +113,17 @@ INPUT_LABELS = (
     # Applicability...
     # Applicable models:
     (1, 8, 1),
-    (1, 8, 2, None),
+    # All (1, 8, 2, N) are processed in below
     # Applicable experiments:
     (1, 9, 1),
-    (1, 9, 2, None),
-)
+    # All (1, 9, 2, N) is processed in below
+]
 
-LABEL_COLUMN = 0  # i.e. index in A-Z of columns as tuple, so "A"
-INPUT_COLUMN = 1  # i.e.  "B"
-
-# Usually <500, but in case of much cell copying and all exps and models listed
-MAX_ROW = 600
+# Extend the list with any potential labels for the questions:
+applicable_models_q2 = [
+    (1, 8, 2, N) for N in range(1, MAX_NUMBER_MODELS_PER_INSTITUTE)]
+applicable_experiments_q2 = [(1, 9, 2, N) for N in range(1, MAX_NUMBER_MIPS)]
+input_labels.extend(applicable_models_q2 + applicable_experiments_q2)
 
 
 def get_machine_tabs(spreadsheet):
@@ -133,22 +146,30 @@ def get_machine_tabs(spreadsheet):
 
 def find_input_cells(spreadsheet_tab):
     """TODO."""
-    input_labels = [
-        ".".join(str(subsec) for subsec in label) for label in INPUT_LABELS]
-
-    # Use a placeholder/default that is unlikely to be specified (not None,
-    # False, etc.) to avoid potential clash with any user input, allowing us to
-    # keep track of input cells that were left empty.
-    label_to_input_cell_mapping = {label: "EMPTY" for label in input_labels}
+    label_values = [
+        ".".join(str(subsec) for subsec in label) for label in input_labels]
+    label_to_input_cell_mapping = {}
 
     # Iterate over label first so stop check on rows via break once label found
-    for label in label_to_input_cell_mapping:
+    for label in label_values:
         for row in spreadsheet_tab.iter_rows(
                 min_row=1, max_row=MAX_ROW, max_col=2):
             label_cell_value = str(row[LABEL_COLUMN].value).strip(" *")
-            if label_cell_value == label or label_cell_value == label:
+            if label_cell_value == label:
                 label_to_input_cell_mapping[label] = row[INPUT_COLUMN]
                 break
+
+        # Remove inapplicable MIPs and experiments:
+        if not label_to_input_cell_mapping.get(label, False):
+            if label.startswith("1.9.2.") or label.startswith("1.8.2."):
+                # Skip these cases
+                print("Inapplicable model or MIP question skipped:", label)
+            else:
+                # Use a placeholder/default that is unlikely to be specified
+                # (not None, False, etc.) to avoid potential clash with any
+                # user input for keeping track of input cells left empty.
+                label_to_input_cell_mapping[label] = "EMPTY"
+
 
     # TODO: need to apply offsets to get actual input cells (often one or
     # two cells down) from the cell adjacent to the sub-section label
